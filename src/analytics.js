10c067d2bf47d8ea934283c201f160a52fe3db7c
mahendramirdhapy-tech <mahendramirdhapy@gmail.com>
2025-11-14 12:25:11 +0530

    Create analytics.js

---
+++ src/analytics.js
@@ -0,0 +1,12 @@
+// analytics.js
+export const initGA = () => {
+  const gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-ELVB6G7Z42';
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-ELVB6G7Z42');
+};
