0db1d9f77ed8d8322b8f99e4cf2f77a28907c156
mahendramirdhapy-tech <mahendramirdhapy@gmail.com>
2025-11-19 21:06:47 +0530

    Update email.js

--- pages/email.js
+++ pages/email.js
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import AdsComponent from '../components/AdsComponent';
 
 export default function EmailGenerator() {
   const [input, setInput] = useState('');
