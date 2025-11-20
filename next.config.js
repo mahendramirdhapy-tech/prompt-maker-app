2fe484ea1a98d093bdc42429be6d091041123ff5
mahendramirdhapy-tech <mahendramirdhapy@gmail.com>
2025-11-20 19:32:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['aipromptmaker.online'],
+    domains: ['alpromptmaker.online'],
   },
   eslint: {
     ignoreDuringBuilds: true,
